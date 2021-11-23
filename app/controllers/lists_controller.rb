606008443a3ebbb61e09b1a0b704b900e3e72f42
Elana Strijbis <ehgestrijbis@gmail.com>
2021-11-23 17:53:41 +1100

    finished the watchlist

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,30 @@
+class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :edit, :destroy]
+  def index
+    @lists = List.all
+  end
+
+  def show
+   @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_parmams)
+    @list.save
+      if @list.save
+        redirect_to_list_path(@list)
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
